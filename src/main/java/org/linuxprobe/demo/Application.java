dca8b0f3d9cdb05c63efd4d2f2cc0ceb765dd118
LarryYu <linuxprobe.org@gmail.com>
2018-12-30 23:47:45 +0800

    update

--- src/main/java/org/linuxprobe/demo/Application.java
+++ src/main/java/org/linuxprobe/demo/Application.java
@@ -1,17 +1,28 @@
 package org.linuxprobe.demo;
 
+import javax.validation.Valid;
+
+import org.linuxprobe.demo.model.Book;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan({ "org.linuxprobe.**.mapper" })
 @ComponentScan("org.linuxprobe.demo**")
+@RestController
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/test")
+	public String test(@Valid Book book) {
+		return "jhe";
 	}
 }
